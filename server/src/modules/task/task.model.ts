584a7c29fd24cdb53cb83c49c7f543fd77105808
vishalmoharikar <vishalmoharikar@gmail.com>
2026-03-24 15:08:08 +0530

    upload file with task creation

--- server/src/modules/task/task.model.ts
+++ server/src/modules/task/task.model.ts
@@ -3,12 +3,14 @@ import mongoose, { Document } from "mongoose";
 export interface ITask extends Document {
     title: string;
     userId: string;
+    fileUrl?: string;
 }
 
 const taskSchema = new mongoose.Schema<ITask>(
     {
         title: { type: String, required: true },
-        userId: { type: String, index: true }
+        userId: { type: String, index: true },
+        fileUrl: { type: String },  
     },
     { timestamps: true }
 );
--- server/src/modules/task/task.validator.ts
+++ server/src/modules/task/task.validator.ts
@@ -1,12 +1,13 @@
 export interface CreateTaskDto {
     title: string;
+    fileUrl?: string;
 }
 
 export const validateCreateTask = (body: any): CreateTaskDto => {
-
     if (!body.title || body.title.length < 3) {
         throw new Error("Title must be at least 3 chars");
     }
-    const { title } = body;
-    return { title: title.trim() };
+    const { title, fileUrl } = body;
+
+    return { title: title.trim(), fileUrl: fileUrl?.trim() };
 };
